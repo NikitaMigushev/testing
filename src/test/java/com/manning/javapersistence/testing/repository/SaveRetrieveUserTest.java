9fdc531f2a72c34b25c60f078df0adaa5f0f00e5
Nikita Migushev <migushev@gmail.com>
2023-08-22 11:21:45 +0300

    Added DirtiesContext

--- src/test/java/com/manning/javapersistence/testing/repository/SaveRetrieveUserTest.java
+++ src/test/java/com/manning/javapersistence/testing/repository/SaveRetrieveUserTest.java
@@ -1,9 +1,10 @@
 package com.manning.javapersistence.testing.repository;
 
 import com.manning.javapersistence.testing.model.AppUser;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 import java.util.List;
 
@@ -11,11 +12,12 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class SaveRetrieveUserTest {
     @Autowired
     private UserRepository userRepository;
 
-    @Test
+    @RepeatedTest(2)
     void saveRetrieve() {
         userRepository.save(new AppUser("User1"));
         List<AppUser> users = userRepository.findAll();
