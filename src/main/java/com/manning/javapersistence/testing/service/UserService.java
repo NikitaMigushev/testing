3be47aee12a8b765cbaee92987d21d2c0b32ad4a
Nikita Migushev <migushev@gmail.com>
2023-08-22 15:15:32 +0300

    Added UserService transactional test

--- src/main/java/com/manning/javapersistence/testing/service/UserService.java
+++ src/main/java/com/manning/javapersistence/testing/service/UserService.java
@@ -2,9 +2,10 @@ package com.manning.javapersistence.testing.service;
 
 import com.manning.javapersistence.testing.model.AppUser;
 import com.manning.javapersistence.testing.repository.UserRepository;
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
 public class UserService {
--- src/test/java/com/manning/javapersistence/testing/repository/TransactionalTest.java
+++ src/test/java/com/manning/javapersistence/testing/repository/TransactionalTest.java
@@ -1,6 +1,7 @@
 package com.manning.javapersistence.testing.repository;
 
 import com.manning.javapersistence.testing.model.AppUser;
+import com.manning.javapersistence.testing.service.UserService;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,38 +19,49 @@ public class TransactionalTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private UserService userService;
+
     @BeforeAll
     static void beforeAll() {
+        System.out.println("===Check Transaction Status Before All===");
         System.out.println("beforeAll, transaction active = " +
                 TransactionSynchronizationManager.isActualTransactionActive());
     }
 
     @BeforeEach
     void beforeEach() {
+        System.out.println("===Check Transaction Status Before Each===");
         System.out.println("beforeEach, transaction active = " +
                 TransactionSynchronizationManager.isActualTransactionActive());
     }
 
     @RepeatedTest(2)
     void storeRetrieve() {
-        userRepository.save(new AppUser("User1"));
-        List<AppUser> users = userRepository.findAll();
+        System.out.println("===Start test===");
+        List<AppUser> users = List.of(new AppUser("User1"), new AppUser("User2"), new AppUser("User3"));
+        userRepository.saveAll(users);
+        var checkUsers = userRepository.findAll();
         assertAll(
-                () -> assertEquals(1, users.size()),
-                () -> assertEquals("User1", users.get(0).getName())
+                () -> assertEquals(3, checkUsers.size()),
+                () -> assertEquals("User1", checkUsers.get(0).getName())
         );
+        userService.saveTransactionally(new AppUser("UserABC"));
+        System.out.println("===Check Transaction Status during Test===");
         System.out.println("end of method, transaction active = " +
                 TransactionSynchronizationManager.isActualTransactionActive());
     }
 
     @AfterEach
     void afterEach() {
+        System.out.println("===Check Transaction Status After Each test===");
         System.out.println("afterEach, transaction active = " +
                 TransactionSynchronizationManager.isActualTransactionActive());
     }
 
     @AfterAll
     static void afterAll() {
+        System.out.println("===Check Transaction Status After All tests===");
         System.out.println("afterAll, transaction active = " +
                 TransactionSynchronizationManager.isActualTransactionActive());
     }
