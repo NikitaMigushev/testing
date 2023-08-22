241b6508ab6441b372a405ebf335f7f9269a4608
Nikita Migushev <migushev@gmail.com>
2023-08-22 15:41:42 +0300

    Added Transaction checks before and after tests

---
+++ src/test/java/com/manning/javapersistence/testing/repository/TransactionsManagementTest.java
@@ -0,0 +1,44 @@
+package com.manning.javapersistence.testing.repository;
+
+import com.manning.javapersistence.testing.model.Log;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.RepeatedTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.transaction.AfterTransaction;
+import org.springframework.test.context.transaction.BeforeTransaction;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+public class TransactionsManagementTest {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private LogRepository logRepository;
+
+    @BeforeTransaction
+    void beforeTransaction() {
+        if (TransactionSynchronizationManager.isActualTransactionActive()) {
+            throw new IllegalStateException("BeforeTransaction check: Test failed because a transaction is still active.");
+        }
+    }
+
+    @RepeatedTest(2)
+    void storeRetrieveLogTest() {
+        logRepository.save(new Log("@TestLog1"));
+        logRepository.save(new Log("@TestLog2"));
+        var checkLog = logRepository.findAll();
+        assertThat(checkLog.size()).isEqualTo(2);
+    }
+
+    @AfterTransaction
+    void afterTransaction() {
+        if (TransactionSynchronizationManager.isActualTransactionActive()) {
+            throw new IllegalStateException("AfterTransactionCheck check: Test failed because a transaction is still active.");
+        }
+    }
+}
